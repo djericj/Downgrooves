283b7d8e7c8e261391d218f14349bd5882b901d1
djericj <djericj@hotmail.com>
2018-09-03 21:27:20 -0500

    progress bar animations

--- src/app/services/player.service.ts
+++ src/app/services/player.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from "@angular/core";
 import { PlayerTrack, IMix, ITunesTrack } from "./interfaces";
+import * as moment from "moment";
+import "moment-duration-format";
+
 @Injectable()
 export class PlayerService {
   private cover;
@@ -14,11 +17,12 @@ export class PlayerService {
     this.player = <HTMLAudioElement>document.getElementById("player2");
     console.log(this.player);
     if (this.player) {
-      console.log(track);
+      //console.log(track);
       this.pause();
       this.load(track);
       this.player.play();
-      console.log(track);
+      this.player.onprogress = function() {};
+      //console.log(track);
     }
   }
   resume() {
@@ -37,6 +41,7 @@ export class PlayerService {
       this.setCover(track);
       $("#mp3_src").attr("src", track.url);
       this.player.load();
+      this.initProgressBar();
     }
   }
   setInfo(track: PlayerTrack) {
@@ -73,6 +78,75 @@ export class PlayerService {
         track.trackTimeMillis,
         track.previewUrl
       );
+    }
+  }
+
+  initProgressBar() {
+    var player = <HTMLAudioElement>document.getElementById("player2");
+    var length = player.duration;
+    var current_time = player.currentTime;
+    var progressbar = <HTMLProgressElement>document.getElementById("seek-obj");
+
+    player.addEventListener("loadedmetadata", function() {
+      var totalLength = formatTime(player.duration);
+      document.getElementById("end-time").innerHTML = totalLength;
+    });
+
+    player.addEventListener("timeupdate", function() {
+      var currentTime = formatTime(player.currentTime);
+      document.getElementById("start-time").innerHTML = currentTime;
+      var pct = player.currentTime / player.duration;
+      document.getElementById("progress-bar").style.width =
+        (pct * 100).toFixed() + "%";
+      //console.log(player.currentTime / player.duration);
+    });
+
+    // calculate total length of value
+    //var totalLength = calculateTotalValue(length);
+    //document.getElementById("end-time").innerHTML = player.duration.toFixed();
+    //document.getElementById("end-time").innerHTML = progressbar.max.toFixed();
+
+    // calculate current value time
+    //var currentTime = calculateCurrentValue(current_time);
+    //document.getElementById("start-time").innerHTML = currentTime;
+
+    //progressbar.value = player.currentTime / player.duration;
+    progressbar.addEventListener("click", seek);
+    progressbar.addEventListener("progress", prog);
+
+    if (player.currentTime == player.duration) {
+      document.getElementById("play-btn").className = "";
+    }
+
+    function formatTime(seconds: number) {
+      let minutes: any = Math.floor(seconds / 60);
+      let secs: any = Math.floor(seconds % 60);
+
+      if (minutes < 10) {
+        minutes = "0" + minutes;
+      }
+
+      if (secs < 10) {
+        secs = "0" + secs;
+      }
+
+      return minutes + ":" + secs;
+    }
+
+    function prog() {
+      var progressbar = <HTMLProgressElement>(
+        document.getElementById("seek-obj")
+      );
+      console.log(progressbar.value.toFixed());
+      document.getElementById(
+        "end-time"
+      ).innerHTML = progressbar.value.toFixed();
+    }
+
+    function seek(event) {
+      var percent = event.offsetX / this.offsetWidth;
+      player.currentTime = percent * player.duration;
+      progressbar.value = percent / 100;
     }
   }
 }
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -4,4 +4,17 @@ interface NodeModule {
   id: string;
 }
 
+import * as moment from "moment";
 
+declare module 'moment' {
+  interface Duration {
+    format(template: string, precision?: string, settings?: DurationFormatSettings): string;
+  }
+
+  interface DurationFormatSettings {
+    template: string;
+    precision: number;
+    trim: boolean;
+    forceLength: boolean;
+  }
+}
